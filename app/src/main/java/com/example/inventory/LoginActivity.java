968d5845fee543a49c844b07771219a0b44a2d7a
Bilal Moreno <bilalmoreno@uma.es>
2018-10-11 11:55:37 +0200

    Añadida DashBoardActivity

--- app/src/main/java/com/example/inventory/LoginActivity.java
+++ app/src/main/java/com/example/inventory/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.example.inventory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_relativelayout);
+        btSignIn = findViewById(R.id.btSignIn);
+        btSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, DashBoardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
